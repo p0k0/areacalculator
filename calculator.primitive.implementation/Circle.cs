9c203249ad6b53af0cbdbfc0796facdb63ce3c57
bulatm <m11e@yandex.ru>
2019-07-12 11:17:03 +0300

    primitive.implementation as circle,triangle added

---
+++ calculator.primitive.implementation/Circle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace calculator.primitive.implementation
+{
+    public class Circle : FigureBase<float>
+    {
+        public override float GetArea()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ calculator.primitive.implementation/Triangle.cs
@@ -0,0 +1,10 @@
+namespace calculator.primitive.implementation
+{
+    public class Triangle : FigureBase<float>
+    {
+        public override float GetArea()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
