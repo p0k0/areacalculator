e9f0ebe038595d088e26e6d8bd53b0de17767745
bulatm <m11e@yandex.ru>
2019-07-12 13:29:41 +0300

    *.primitive.implementation.factory.tests added

---
+++ calculator.primitive.implementation.factory.tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace calculator.primitive.implementation.factory.tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
